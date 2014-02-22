a9c98233ed4d8b81d875f302524250d960604284
dmitrinesterenko <dmitri@sentimentalistmag.com>
2014-02-22 17:20:47 -0500

    Host own version of jquery ui

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(:default, Rails.env)
 module HowmuchbrooklyncanyoubuyRails
   class Application < Rails::Application
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
